c38a052c6b0c0a3d8d5306d3dd288c7e024db431
Bhuvanesh <70952578+Bhuvanesh16s25@users.noreply.github.com>
2020-12-12 16:28:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground,ball;
+var binImg,bin;
+
+function preload(){
+    binImg = loadImage("dustbingreen.png");
+}
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    ground = new Ground();
+    crumpledPaper = new Paper();
+
+    bin = createSprite(964,520,20,20);
+    bin.addImage(binImg);
+    bin.isStatic = true;
+    bin.scale = 0.45;
+
+    bin1 = new Dustbin(902,505,10,120);
+    bin2 = new Dustbin(962,565,130,10);
+    bin3 = new Dustbin(1024,505,10,120);
+}
+
+function draw(){
+    background("pink");
+    Engine.update(engine);
+
+
+    ground.display();
+    crumpledPaper.display();
+    // bin1.display();
+    // bin2.display();
+    // bin3.display(); 
+      
+    drawSprites();
+}
+
+function keyPressed(){
+    if(keyCode === UP_ARROW){
+        Matter.Body.applyForce(crumpledPaper.body,crumpledPaper.body.position,{x:74,y:-75});
+    }
+}
